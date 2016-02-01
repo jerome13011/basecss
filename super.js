c81ba063b8bac80cb851f9df3db37e3a657c2ce4
[jerome13011] <[fanniojerome@free.fr]>
2016-02-01 08:54:25 +0100

    teste ssh

--- super.js
+++ super.js
@@ -1,1 +1,14 @@
-$.getJSON("http://jerome13011.github.io/basecss/main.json",function(données){
+$(document).ready(function()
+{
+  $.getJSON("http://jerome13011.github.io/basecss/main.json"), function()
+{
+}
+});
+$(window).scroll(function() {
+if ($(this).scrollTop() > 2){
+    $('header').addClass("sticky");
+  }
+  else{
+    $('header').removeClass("sticky");
+  }}
+  );
